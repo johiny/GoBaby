4e9044fa3b137ae7ba02950fc170326b0423f3db
johiny <johiny7@live.com>
2024-04-30 21:06:22 -0500

    User struct modified

--- internal/models/user.model.go
+++ internal/models/user.model.go
@@ -2,6 +2,8 @@ package models
 
 type User struct {
 	UserName string `bson:"username"`
+	Email    string `bson:"email"`
+	Password string `bson:"password"`
 	Logs     []Log  `bson:"logs"`
 	Id       int    `bson:"_id, omitempty"`
 }
